28fee74039d4fe99313f40071e8758920b829525
lspar <21lsparks@athenian.org>
2020-11-17 17:15:08 -0800

    It is now finished

--- my_code.py
+++ my_code.py
@@ -1,3 +1,18 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
+cities = {}
+while True:
+    print("When done, type 'done'")
+    a = input("Pick an American city: ")
+    if a == "done":
+        break
+    b = input("What state is that city in?: ")
+    if b == "done":
+        break
+    cities[a] = b
+
+
+
+for key_name, value_name in cities.items():
+    print(key_name, value_name)
